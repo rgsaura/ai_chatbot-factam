f622932c1ba2c3f3b309bf3d8593b0074e9f9016
Rodrigo Garcia <16281075+rgsaura@users.noreply.github.com>
2023-03-28 17:24:24 +0200

    init

---
+++ utils/makechain.ts
@@ -0,0 +1,64 @@
+import { OpenAIChat } from 'langchain/llms';
+import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
+import { PineconeStore } from 'langchain/vectorstores';
+import { PromptTemplate } from 'langchain/prompts';
+import { CallbackManager } from 'langchain/callbacks';
+
+const CONDENSE_PROMPT =
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`);
+
+// Q: Quién eres? A: Soy FACTAM IA y estoy aquí para responder tus respuestas. Estoy entrenado en una gran cantidad de artículos médicos especializados. Intentaré darte la información más precisa y útil dentro de mis capacidades pero puede que en algunos casos me equivoque, por favor toma mis respuestas como un punto de referencia y consulta a tu médico antes de tomar a cabo una decisión importante.
+
+
+const QA_PROMPT = PromptTemplate.fromTemplate(
+  `You are "FACTAM IA", an AI assistant providing helpful advice. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
+You should only provide hyperlinks that reference the context below. Do NOT make up hyperlinks.
+If you can't find the answer in the context below, just say "Hmm, no estoy seguro." Don't try to make up an answer. You will always answer in Spanish. 
+
+
+
+Question: {question}
+=========
+{context}
+=========
+Answer in Markdown:`,
+);
+
+export const makeChain = (
+  vectorstore: PineconeStore,
+  onTokenStream?: (token: string) => void,
+) => {
+  const questionGenerator = new LLMChain({
+    llm: new OpenAIChat({ temperature: 0 }),
+    prompt: CONDENSE_PROMPT,
+  });
+  const docChain = loadQAChain(
+    new OpenAIChat({
+      temperature: 0,
+      modelName: 'gpt-4', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+      streaming: Boolean(onTokenStream),
+      callbackManager: onTokenStream
+        ? CallbackManager.fromHandlers({
+            async handleLLMNewToken(token) {
+              onTokenStream(token);
+              console.log(token);
+            },
+          })
+        : undefined,
+    }),
+    { prompt: QA_PROMPT },
+  );
+
+  return new ChatVectorDBQAChain({
+    vectorstore,
+    combineDocumentsChain: docChain,
+    questionGeneratorChain: questionGenerator,
+    returnSourceDocuments: true,
+    k: 2, //number of source documents to return
+  });
+};
