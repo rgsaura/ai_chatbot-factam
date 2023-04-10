fe96a20618f62bb17493897eb3a9c1c3086e73fd
Rodrigo Garcia <16281075+rgsaura@users.noreply.github.com>
2023-04-10 10:22:13 +0200

    gitignore

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -6,7 +6,7 @@ import { CustomPDFLoader } from '@/utils/customPDFLoader';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 
 /* Name of directory to retrieve files from. You can change this as required */
-const filePath = 'docs/MorseVsFrederick.pdf';
+const filePath = 'docs/Book.pdf';
 
 export const run = async () => {
   try {
@@ -19,8 +19,8 @@ export const run = async () => {
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
+      chunkSize: 200,
+      chunkOverlap: 100,
     });
 
     const docs = await textSplitter.splitDocuments(rawDocs);
